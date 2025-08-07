b88f18201b5443d37ddbf880b79e4a14a54e15ca
HocTran <tranvanhoc.tb@gmail.com>
2025-08-07 20:34:26 +0900

    Updated nginx config

--- src/Modules/PingPong/Routes/api.php
+++ src/Modules/PingPong/Routes/api.php
@@ -4,12 +4,12 @@ use Modules\PingPong\Http\Controllers\Api\v1\PingPongController as PingPongV1Con
 use Modules\PingPong\Http\Controllers\Api\V2\PingPongController as PingPongV2Controller;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('hmac.auth')->group(function (): void {
-    Route::group(['prefix' => 'v1'], function () {
+//Route::middleware('hmac.auth')->group(function (): void {
+    Route::group(['prefix' => '1'], function () {
         Route::resource('ping-pong', PingPongV1Controller::class, ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
     });
 
-    Route::group(['prefix' => 'v2'], function () {
+    Route::group(['prefix' => '2'], function () {
         Route::resource('ping-pong', PingPongV2Controller::class, ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
     });
-});
+//});
